481d818780f6bbd2a742d078caf51491175a6663
mehulsingh1010 <mehulsingh076@gmail.com>
2025-10-28 12:14:30 +0530

    Rust init

---
+++ week-3-Rust/hello_rust/src/main.rs
@@ -0,0 +1,10 @@
+fn main() {
+    println!("Hello, world!");
+}
+
+//snake_case is followed in rust generally for function and variable names
+//CamelCase is followed for struct and enum names
+//CONSTANT_CASE is followed for constants and statics
+
+
+//so RUsT: hello_world (snake case), JS: helloWorld (camel case)
